81de3a796ef9e826c4371682c7080ec2a57bce19
vajrender kumar <vkm240@nyu.edu>
2016-08-30 23:26:55 -0500

    commit one

---
+++ src/main/java/io/egen/hello/App.java
@@ -0,0 +1,12 @@
+package io.egen.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(App.class, args);
+    }
+}
